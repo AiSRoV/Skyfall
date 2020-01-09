30ebd8c06f23233c08ca271384d7a2fc3913aa1f
Skyfall-6969 <Sky_full_off_stars@mail.ru>
2020-01-09 21:26:36 +0300

    09012020

---
+++ Проекты/27. Лутбокс.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream> // cout, cin
+#include <time.h>   // time
+#include <stdlib.h> // srand
+
+using namespace std;
+
+enum Element { FIRE, AIR, WATER, EARTH };
+enum ItemType { SWORD, BOW, SPELL };
+
+struct Sword {
+	int damage;
+};
+
+struct Bow {
+	int damage;
+	int charge_time;
+	double distance;
+};
+
+struct Spell {
+	int damage;
+	Element element;
+	double distance;
+};
+
+struct Item {
+	ItemType itemtype;
+	union { // объединение, нужно для экономии памяти
+		Sword sword;
+		Bow bow;
+		Spell spell;
+	} item;
+};
+
+Item getRandomItem(); // Создаёт и возвращает предмет со случайными характеристиками
+void printItem(Item loot); // Выводит на экран информацию о предмете
+
+int main() {
+	Item loot[5];
+	int i;
+
+	srand((unsigned)time(NULL));
+
+	for (i = 0; i < 5; ++i) {
+		loot[i] = getRandomItem();
+		printItem(loot[i]);
+	}
+
+	return 0;
+}
+
+Item getRandomItem() {
+	Item loot;
+	int predmet = rand() % 3, // что за предмет (меч=0, лук=1, заклин=2)
+		magiya = rand() % 4; // огонь=0, воздух=1, вода=2, земля=3
+
+	switch (predmet) {
+	case 0: loot.itemtype = SWORD;
+		loot.item.sword.damage = rand() % 1000;
+		break;
+	case 1: loot.itemtype = BOW;
+		loot.item.bow.damage = rand() % 1000;
+		loot.item.bow.charge_time = rand() % 100;
+		loot.item.bow.distance = rand() % 100;
+		break;
+	case 2: loot.itemtype = SPELL;
+		loot.item.spell.damage = rand() % 1000;
+		loot.item.spell.distance = rand() % 100;
+		switch (magiya) {
+		case 0: loot.item.spell.element = FIRE; break;
+		case 1: loot.item.spell.element = AIR; break;
+		case 2: loot.item.spell.element = WATER; break;
+		case 3: loot.item.spell.element = EARTH; break;
+		}
+		break;
+	}
+
+	return loot;
+}
+
+void printItem(Item loot) {
+	switch (loot.itemtype) {
+	case SWORD: cout << "---SWORD---" << "\n";
+		cout << "Damage: " << loot.item.sword.damage << "\n\n";
+		break;
+	case BOW:   cout << "---BOW---" << "\n";
+		cout << "Damage: " << loot.item.bow.damage << "\n";
+		cout << "Charge Time: " << loot.item.bow.charge_time << "\n";
+		cout << "Distance: " << loot.item.bow.distance << "\n\n";
+		break;
+	case SPELL: cout << "---SPELL---" << "\n";
+		cout << "Damage: " << loot.item.spell.damage << "\n";
+		cout << "Element: ";
+		switch (loot.item.spell.element) {
+		case FIRE:  cout << "Fire" << "\n"; break;
+		case AIR:   cout << "Air" << "\n";  break;
+		case WATER: cout << "Water" << "\n"; break;
+		case EARTH: cout << "Earth" << "\n"; break;
+		}
+		cout << "Distance: " << loot.item.spell.distance << "\n\n";
+		break;
+
+	}
+}
+
+
---
+++ Проекты/29. Аргументы команднй стоки.cpp
@@ -0,0 +1,80 @@
+﻿// 29. Аргументы команднй стоки.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdlib.h> // system
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int i;
+	// state == 1 2 4 8 // (-help)  (-n <name>) (-b) (-w)
+	int state[4] = { 0 };
+	int result,
+		index;
+	char s[255];
+
+	if (argc > 1) { // количество аргументов в командной строке == 1 -> имя программы
+		for (i = 1; i < argc; ++i) { // смотрим переданные аргументы в командной строке
+			if (strcmp(argv[i], "-help") == 0) { //
+				state[0] = 1;
+			}
+			if (strcmp(argv[i], "-n") == 0) {
+				state[1] = 2;
+				index = i;
+			}
+			if (strcmp(argv[i], "-b") == 0) {
+				state[2] = 4;
+				index = i;
+			}
+			if (strcmp(argv[i], "-w") == 0) {
+				state[3] = 8;
+			}
+		}
+
+		if (state[0]) {
+			result = 1;
+		}
+		else {
+			result = state[0] + state[1] + state[2] + state[3];
+		}
+
+		switch (result) {
+		/*-help*/  case 1: cout << "-help      'show help'" << "\n";
+			cout << "-n <name>  'hello, <name>'" << "\n";
+			cout << "-b <name>  'poka, <name>'" << "\n";
+			cout << "-w         'asks <name>'" << "\n";
+			cout << "           'can use -w -n'" << "\n\n";
+			break;
+		/*-n name*/case 2: if (index + 1 == argc) { // если аргументов больше нет
+			cout << "error! use '-help' for help" << "\n";
+		}
+		else {
+			cout << "privet, " << argv[index + 1] << "\n";
+		}
+		break;
+		/*-b name*/case 4: if (index + 1 == argc) { // если аргументов больше нет
+			cout << "error! use '-help' for help" << "\n";
+		}
+		else {
+			cout << "poka, " << argv[index + 1] << "\n";
+		}
+		break;
+		/*-w*/     case 8: cout << "kak vas zovut? --> ";
+			cin.getline(s, 255, '\n');
+			break;
+		/*-w -n*/  case 10:cout << "kak vas zovut? --> ";
+			cin.getline(s, 255, '\n');
+			cout << "privet, " << s << "\n";
+			break;
+		}
+
+	}
+	else {
+		cout << "no arguments!\n";
+	}
+
+	system("pause");
+	return 0;
+}
